20e70edd1d7163013825f6058c15e60ce0b66730
bomkyu <a01090762806@gmail.com>
2023-07-10 19:10:06 +0900

    Working_write.jsx 친구추가 중간푸시

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
-
-const Modal = ({value : { isOpen, type }, close}) => {
+const Modal = ({value : { isOpen, type }, close, data, onClick}) => {
+  console.log(data);
   return (
     <div className={`modal-wrap ${isOpen && 'active'} ${type}`}>
         <div className="modal-contain">
             <div className="modal-header" onClick={close}></div>
             <div className="modal-content">
-                
+              { 
+                type === 'h_80' ? 
+                data && (
+                  data.map((param)=> 
+                    <dl className={`friend-list ${param.selected ? 'active': ''}`} key={param.id} onClick={()=>onClick(param.id)}>
+                      <dt>
+                        <div className="img-wrap">
+                          <div className="img-thumb" style={{backgroundImage : `url(${param.imgUrl ? param.imgUrl : './images/img_user.png'})`}}></div>
+                        </div>
+                      </dt>
+                      <dd>
+                        <p className="nick-name">{param.userName}</p>
+                        <p className="user-name">{param.userNickName}</p>
+                      </dd>
+                    </dl>
+                  )
+                  
+                )
+                :
+                <>나는 달력이야!</>
+              }
             </div>
       </div>
     </div>
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,10 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import Input from '../UI/Input'
 import Modal from '../Modal/Modal'
+import {FriendSelect } from '../Api/firebase';
 const Write = () => {
+    const sessionId = sessionStorage.getItem('userID');
+    const [friend, setFriend] = useState([]);
+    const [selectFriend, setSelectFriend] = useState([]);
     const [modal, setModal] = useState({isOpen : false, type : ''});
     const [inputs, setInputs] = useState({ goDay : "2023-07-05", comeDay: "2023-08-05", friends: '', placeSearch: '', placePrice: '', taxPrice: ''});
-    const {goDay, comeDay, friends, placeSearch, placePrice, taxPrice} = inputs
+    const {goDay, comeDay, friends, placeSearch, placePrice, taxPrice} = inputs;
+
+    const fetchData = async () => {
+      let usersData = await FriendSelect(`${sessionId}`,handleFriendData);
+      filterFriend(usersData);
+    };
+
+    //snapShot에서 Event가 있을 경우 콜백되는 함수
+    const handleFriendData = (data) => {
+        filterFriend(data);
+    };
+
+    const filterFriend = (data) => {
+        const filterFriends = data
+        .filter((param) => param.request?.status === 'accept' && param.request?.status !== null)
+        .map((param) => ({ ...param, selected: false}));
+        setFriend(filterFriends);
+    }
+
+    useEffect(()=> {
+        fetchData();
+    }, [])
     
     const openModal = (type) => {
         setModal((prevState) => ({
@@ -26,10 +51,24 @@ const Write = () => {
             [id] : value
         })
     }
+
+    const onClickHandler = (id) => {
+        console.log(id);
+        const updatedFriends = friend.map((param) => {
+            if (param.id === id) {
+              return {
+                ...param,
+                selected: !param.selected  // selected 프로퍼티를 반전시킴
+              };
+            }
+            return param;
+          });
+        setFriend(updatedFriends);
+    }
     
     const placeSearchClick = () => {
         if(placeSearch === ''){
-            return alert('검색어를 입력해주세요. 제발..')
+            return alert('검색어를 입력해주세요.')
         }
     }
 
@@ -38,13 +77,11 @@ const Write = () => {
         alert(`goDay : ${goDay}, comeDay : ${comeDay}, friends : ${friends}, placeSearch : ${placeSearch}, placePrice : ${placePrice}, taxPrice : ${taxPrice}`);
     }
 
+    //선택된 친구 필터링하는 변수
+    const filteredFriends = friend.filter((param) => param.selected);
+
     return (
         <>
-            <LoadScript
-                googleMapsApiKey="AIzaSyAQRIT2tpNRKu9hleY2y7XHil4qxRyFGv8"
-                libraries={['places']}
-            >
-            </LoadScript>
             <div className="content-wrap">
                 <form method="post" onSubmit={submit}>
                 <ul className="date-selector">
@@ -66,9 +103,23 @@ const Write = () => {
                 <section className="sec1">
                     <h2 className="title">인원</h2>
                     <a className="btn btn-st1" onClick={()=>openModal('h_80')}><span className="plus"></span></a>
-                    <ul className="user_list mt-20">
-                        
-                    </ul>
+                    {
+                        filteredFriends.length > 0 ?
+                        <ul class="user_list" style={{marginTop:'20px'}}>
+                        {
+                            filteredFriends.map((param) => (
+                                <li onClick={()=>onClickHandler(param.id)}>
+                                    <div className="img-wrap">
+                                        <div className="img-thumb" style={{backgroundImage : `url(${param.imgUrl ? param.imgUrl : './images/img_user.png'})`}}></div>
+                                    </div>
+                                    <p>{param.userName}</p>
+                                </li>
+                            ))
+                        }
+                        </ul>
+                        :
+                        <p>친구를 추가해 주세요!</p>
+                    }
                     <input type="text" id="friends" name="friends" style={{display:'none'}}/>
                 </section>
                 <section className="sec2">
@@ -97,7 +148,7 @@ const Write = () => {
                 <button className="btn btn-st1" type="submit"><p>등록</p></button>
             </form>
             </div>  
-            <Modal value={modal} close={closeModal}/>
+            <Modal value={modal} data={friend} close={closeModal} onClick={onClickHandler}/>
         </>
     )
 }
